86c38d9a8d4eef40d8213fc894049f3c8e1bf708
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2023-03-08 21:48:01 +0600

    done

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -3,6 +3,25 @@ import EditTask from './EditTask'
 
 const Todo = ({task, index, taskList,setTaskList }) => {
 const [time,setTime] = useState(0);
+const [running, setRunning] = useState(false)
+
+
+  useEffect(()=>{
+    let interval;
+    if(running){
+    interval = setInterval(()=>{
+      setTime((prevTime)=>prevTime + 10)
+    },10)
+  }
+    else if(!running){
+        clearInterval(interval)
+    }
+    return ()=>clearInterval(interval)
+
+  },[running])
+
+
+
 
   const handleDelete =(itemID)=>{
     let removeIndex = taskList.indexOf(task)
@@ -22,8 +41,8 @@ const [time,setTime] = useState(0);
 
 </div>
         <p className='text-lg py-2'>{task.taskDescription}</p>
-        <div>
-          <div>
+        <div className='w-full flex flex-col items-center justify-center'>
+          <div className='text-xl font-semibold py-3'>
 
               <spna>{("0"+ Math.floor((time/3600000)%24)).slice(-2)}:</spna>
 
@@ -33,6 +52,20 @@ const [time,setTime] = useState(0);
 
               <spna>{("0"+Math.floor((time/10)%100)).slice(-2)}</spna>
 
+
+          </div>
+          <div className='py-4 flex flex-row justify-evenly gap-4'>
+              {running ? (<button className='border rounded-lg bg-rose-400 py-1 px-3'>
+                Stop
+              
+              </button>):(<button className='border rounded-lg bg-lime-400  py-1 px-3'>
+              
+              Start
+              </button>
+                
+                )}
+                <button className='border rounded-lg py-1 px-3 bg-cyan-400 hover:opacity-90 hover:duration-100'> Reset </button>
+              
 
           </div>
           <div>
